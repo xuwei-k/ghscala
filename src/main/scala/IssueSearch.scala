5cc2ca560ce5099f57688ccf69fd1abfa50e86c2
xuwei-k <6b656e6a69@gmail.com>
2012-06-19 13:52:32 +0900

    add comments

--- src/main/scala/IssueSearch.scala
+++ src/main/scala/IssueSearch.scala
@@ -3,13 +3,13 @@ package com.github.xuwei_k.ghscala
 case class IssueSearch(
   pull_request_url :Option[String],
   state            :String,
-  user             :String,
+  user             :String, // TODO optional ? https://api.github.com/legacy/issues/search/harrah/xsbt/closed/scala
   updated_at       :String,
   labels           :List[String],
   position         :Double,
   closed_at        :DateTime,
   title            :String,
-  gravatar_id      :String,
+  gravatar_id      :String, // TODO optional ?
   votes            :Long,
   number           :Long,
   html_url         :String,
