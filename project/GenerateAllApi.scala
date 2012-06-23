44b7144e28c228528320fb9c55e51ca0c8d2afae
xuwei-k <6b656e6a69@gmail.com>
2012-06-23 21:53:01 +0900

    debug and test codes. fix all method

--- project/GenerateAllApi.scala
+++ project/GenerateAllApi.scala
@@ -2,24 +2,24 @@ import sbt._,Keys._
 
 object GenerateAllApi{
 
-  val m = "repos followers issues watched searchRepo searchIssues  pulls orgs"
+  val m = "issues watched searchRepo downloads watchers searchIssues pulls" // TODO
 
-  val methods = "refs downloads forks watchers collaborators".split(" ").toList
+  val methods = "downloads followers repos refs forks collaborators orgs".split(" ").toList
 
-  val methods2 = "issueEvents comments"
+  val methods2 = "issueEvents comments" // TODO
 
   def task(dir:File):Seq[File] = {
     val str =
     Iterator(
       "package com.github.xuwei_k.ghscala\n",
-      "object All{\n",
+      "trait All{\n",
       methods.map{m =>
-        "  val " + m + "All = Common.all(GhScala." + m + ")"
+        "  val " + m + " = Common.all(Core._" + m + ")"
       }.mkString("\n\n"),
       "}"
     ).mkString("\n")
     println(str)
-    val file = dir / "all.scala"
+    val file = dir / "All.scala"
     IO.write(file,str)
     Seq(file)
   }
--- src/main/scala/Github.scala
+++ src/main/scala/Github.scala
@@ -1,18 +1,11 @@
 package com.github.xuwei_k.ghscala
 
 import Common._
+import net.liftweb.json._
 
 trait GhScala{
 
   def repo(user:String,repo:String):Repo = single[Repo]("repos",user,repo)
-
-  val repos = (page:Int) => (user:String) => listRequest[Repo]("users",user,"repos")()(page)
-
-  val refs = (page:Int) => {
-    listRequest[Ref]("repos",_:String,_:String,"git/refs")()(page)
-  }.tupled
-
-  val followers = (user:String) => listRequest[User]("users",user,"followers")()(_)
 
   val searchRepo = (query:String) => {
     val json = getJson("legacy/repos/search",query)
@@ -38,26 +31,9 @@ trait GhScala{
   def issueEvents(user:String,repo:String):List[IssueEvent2] =
     list[IssueEvent2]("repos",user,repo,"issues/events")
 
-  val downloads = (page:Int) => {
-    listRequest[Download]("repos",_:String,_:String,"downloads")()(page)
-  }.tupled
-
   def download(user:String,repo:String,id:Long):Download = single[Download]("repos",user,repo,"downloads",id.toString)
 
-  val forks = (page:Int) => {
-    listRequest[Repo]("repos",_:String,_:String,"forks")()(page)
-  }.tupled
-
-  // TODO error if contains organization
-  val watchers = (page:Int) => {
-    listRequest[User]("repos",_:String,_:String,"watchers")()(page)
-  }.tupled
-
   val watched = (user:String) => list[Repo]("users",user,"watched")
-
-  val collaborators = (page:Int) => {
-    listRequest[User]("repos",_:String,_:String,"collaborators")()(page)
-  }.tupled
 
   def comments(user:String,repo:String):List[Comment] = list[Comment]("repos",user,repo,"comments")
 
@@ -72,12 +48,20 @@ trait GhScala{
 
   def pulls(user:String,repo:String,state:State = Open):List[Pull] = listWithParams[Pull]("repos",user,repo,"pulls")("state" -> state.name )
 
-  val orgs = (page:Int) => {
-    listRequest[Org]("users",_:String,"orgs")()(page)
-  }
-
   def org(orgName:String):Organization = single[Organization]("orgs",orgName)
+
+  def rateLimit():Option[Long] = {
+    for{
+      JInt(j) <- getJson("rate_limit") \ "rate" \ "remaining"
+    } yield j
+  }.headOption.map{_.longValue}
+
+  val watchers: PARAM => (List[User],List[Org]) =
+    (params:PARAM ) => {
+      val list = Iterator.from(1).map(Core._watchers(_)(params)).takeWhile{case (a,b) => a.size + b.size == DEFAULT_PER_PAGE }.toList
+      list.flatMap{_._1} -> list.flatMap{_._2}
+    }
 }
 
-object GhScala extends GhScala
+object GhScala extends GhScala with All
 
