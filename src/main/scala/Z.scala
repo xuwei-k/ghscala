77f39423e1657420904c84acd0793de604a12184
xuwei-k <6b656e6a69@gmail.com>
2014-02-24 08:44:43 +0900

    mapSuspensionFreeC

--- src/main/scala/Z.scala
+++ src/main/scala/Z.scala
@@ -27,5 +27,11 @@ object Z {
   implicit def freeCMonad[S[_]]: Monad[({type λ[α] = FreeC[S, α]})#λ] =
     Free.freeMonad[({type λ[α] = Coyoneda[S, α]})#λ]
 
+  def mapSuspensionFreeC[F[_], G[_], A](c: FreeC[F, A], f: F ~> G): FreeC[G, A] = {
+    type CoyonedaG[A] = Coyoneda[G, A]
+    c.mapSuspension[CoyonedaG](new (({type λ[α] = Coyoneda[F, α]})#λ ~> CoyonedaG){
+      def apply[A](a: Coyoneda[F, A]) = a.trans(f)
+    })
+  }
 }
 
