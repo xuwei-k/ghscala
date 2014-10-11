70f1ff18fad326a1b8732fe4a479ec565ac739ca
xuwei-k <6b656e6a69@gmail.com>
2014-10-11 12:57:00 +0900

    joda-time 2.5

--- project/Build.scala
+++ project/Build.scala
@@ -151,7 +151,7 @@ object build extends Build {
     libraryDependencies ++= Seq(
       "com.github.xuwei-k" %% "httpz" % httpzVersion,
       "com.github.xuwei-k" %% "httpz-scalaj" % httpzVersion % "test",
-      "joda-time" % "joda-time" % "2.4",
+      "joda-time" % "joda-time" % "2.5",
       "org.joda" % "joda-convert" % "1.6",
       "commons-codec" % "commons-codec" % "1.6"
       // latest commons-codec is 1.9 but "httpclient" % "4.3.3" still depends on 1.6
