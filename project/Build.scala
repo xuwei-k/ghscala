b60c752db508d951f2d83affa0cc0f96798eb1c9
xuwei-k <6b656e6a69@gmail.com>
2016-07-09 15:01:18 +0900

    joda-time 2.9.4

--- project/Build.scala
+++ project/Build.scala
@@ -166,7 +166,7 @@ object build extends Build {
     libraryDependencies ++= Seq(
       "com.github.xuwei-k" %% "httpz" % httpzVersion,
       "com.github.xuwei-k" %% "httpz-scalaj" % httpzVersion % "test",
-      "joda-time" % "joda-time" % "2.9.2",
+      "joda-time" % "joda-time" % "2.9.4",
       "org.joda" % "joda-convert" % "1.7",
       "commons-codec" % "commons-codec" % "1.10"
     )
